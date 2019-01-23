d0e946f5b69a946d48f7d8de3a207a37acccfe25
Benedikt Böhme <benedikt.boehme@fb-pro.com>
2019-01-23 10:51:43 +0100

    removed trailing whitespace

--- IIS10Audit/IIS10Audit.psm1
+++ IIS10Audit/IIS10Audit.psm1
@@ -2360,7 +2360,7 @@ function Test-IISAES128Disabled {
 	catch {
 		# do anything here
 	}
-	
+
 	# If the key/value is not present,Triple AES 128/128 Cipher is disabled
 
 	New-Object -TypeName AuditInfo -Property @{
