669cdfcf3d4602422297f8a66707cd7995ba39d1
Benedikt Böhme <benedikt.boehme@fb-pro.com>
2019-10-21 10:50:45 +0200

    added windows 10 gdpr audit manifest

--- Windows10GDPRAudit/Windows10GDPRAudit.psm1
+++ Windows10GDPRAudit/Windows10GDPRAudit.psm1
@@ -1,4 +1,37 @@
-﻿using module ATAPHtmlReport
+﻿<#
+BSD 3-Clause License
+
+Copyright (c) 2019, FB Pro GmbH
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#>
+
+
+using module ATAPHtmlReport
 
 function Test-Windows10_GDPR_MS_1 {
 	[CmdletBinding()]
@@ -2548,16 +2581,16 @@ function Convert-ToAuditInfo {
 }
 
 
-function Get-Windows10_GDPRHtmlReport {
+function Get-Windows10GDPRHtmlReport {
 	<#
     .Synopsis
         Generates an audit report in an html file.
     .Description
-        The Get-Windows10_GDPRHtmlReport cmdlet collects by default data from the current machine to generate an audit report.
+        The Get-Windows10GDPRHtmlReport cmdlet collects by default data from the current machine to generate an audit report.
     .Parameter Path
         Specifies the relative path to the file in which the report will be stored.
     .Example
-        C:\PS> Get-Windows10_GDPRHtmlReport -Path "MyReport.html"
+        C:\PS> Get-Windows10GDPRHtmlReport -Path "MyReport.html"
     #>
 
 	[CmdletBinding()]
@@ -2582,5 +2615,5 @@ function Get-Windows10_GDPRHtmlReport {
 		}
 	)
 
-	Get-ATAPHtmlReport -Path $Path -Title "Windows10_GDPR Audit Report" -ModuleName "Windows10_GDPRAudit" -BasedOn $sections.Title -Sections $sections -DarkMode: $DarkMode
+	Get-ATAPHtmlReport -Path $Path -Title "Windows 10 GDPR Audit Report" -ModuleName "Windows10GDPRAudit" -BasedOn $sections.Title -Sections $sections -DarkMode:$DarkMode
 }
