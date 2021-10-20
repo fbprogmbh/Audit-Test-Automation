295c29fb88d9b19d89cc3e7d9cf8e6f4435e147d
Greg Hicks <greghicks@iinet.net.au>
2021-10-20 18:44:20 +1000

    Updated Detection of No Managed Code in IIS

--- ATAPAuditor/Reports/Microsoft IIS10.ps1
+++ ATAPAuditor/Reports/Microsoft IIS10.ps1
@@ -1233,7 +1233,7 @@ function Test-IISDotNetTrustLevel {
 
 			# medium trust level should be set in .NET 2.*, but not in later versions
 			if (($appPoolVersion -like "v2.*" -and $level -ne "medium") `
-			-or ($appPoolVersion -notlike "v4.*" -and appPoolVersion -notlike "no*")) {
+			-or ($appPoolVersion -notlike "v4.*" -and $appPoolVersion -notlike "no*")) {
 				$message = "TrustLevel set to $level"
 				$audit = "False"
 			}
