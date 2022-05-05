91ef2b4f2bfa56ef6b4b3f26fe5b30213118cfd4
Steffen Winternheimer <s.winternheimer@fb-pro.com>
2022-05-05 19:54:23 +0200

    Update AuditGroups Windows 10 CIS 1.11.0 Add AuditGroups Windows 11 CIS 1.0.0 Add AuditGroups Windows Server 2022 CIS 1.0.0 Update Readme

---
+++ ATAPAuditor/AuditGroups/Microsoft Windows 10-CIS-1.11.0#SecurityOptions.ps1
@@ -0,0 +1,30 @@
+﻿[AuditTest] @{
+    Id = "2.3.1.1"
+    Task = "(L1) Ensure 'Accounts: Administrator account status' is set to 'Disabled'"
+    Test = {
+    }
+}
+[AuditTest] @{
+    Id = "2.3.1.3"
+    Task = "(L1) Ensure 'Accounts: Guest account status' is set to 'Disabled'"
+    Test = {
+    }
+}
+[AuditTest] @{
+    Id = "2.3.1.5"
+    Task = "(L1) Configure 'Accounts: Rename administrator account'"
+    Test = {
+    }
+}
+[AuditTest] @{
+    Id = "2.3.1.6"
+    Task = "(L1) Configure 'Accounts: Rename guest account'"
+    Test = {
+    }
+}
+[AuditTest] @{
+    Id = "2.3.11.6"
+    Task = "(L1) Ensure 'Network security: Force logoff when logon hours expire' is set to 'Enabled'"
+    Test = {
+    }
+}
