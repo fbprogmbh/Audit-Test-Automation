9ecf12048cce1392b3eac5155c193cbbe85b2e6a
Steffen Winternheimer <s.winternheimer@fb-pro.com>
2022-09-07 15:01:26 +0200

    Remove BSI BPOL and CyberGovAu Report from Windows 10 Report

--- ATAPAuditor/Reports/Microsoft Windows 10.ps1
+++ ATAPAuditor/Reports/Microsoft Windows 10.ps1
@@ -4,7 +4,6 @@
 	BasedOn = @(
 		"CIS Microsoft Windows 10 Enterprise Release 21H1 Benchmark, Version: 1.12.0, Date: 2022-02-15"
 		"DISA Windows 10 Security Technical Implementation Guide, Version: V1R16, Date: 2019-10-25"
-		"CYBERGOVAU Hardening Microsoft Windows 10 version 21H1 Workstations, Version: 10.2020, Date 2020-10-01"
 		"Microsoft Security baseline (FINAL) for Windows 10, Version: 21H1, Date: 2021-05-18"
 		"BSI SiM-08202 Client unter Windows 10, Version: 1, Date: 2017-09-13"
         "Configuration Recommendations for Hardening of Windows 10 Using Built-in Functionalities: Version 1.3, Date: 2021-05-03"
@@ -74,28 +73,6 @@
 				[ReportSection] @{
 					Title = "Registry Permissions"
 					AuditInfos = Test-AuditGroup "Microsoft Windows 10-DISA-V1R16#RegistryPermissions"
-				}
-			)
-		}
-		[ReportSection] @{
-			Title = 'CyberGovAu Benchmarks'
-			Description = 'This section contains the CyberGovAu Benchmark results.'
-			SubSections = @(
-				[ReportSection] @{
-					Title = 'Registry Settings/Group Policies'
-					AuditInfos = Test-AuditGroup "Microsoft Windows 10-CyberGovAu-10.2020#RegistrySettings"
-				}
-				# [ReportSection] @{
-				# 	Title = 'User Rights Assignment'
-				# 	AuditInfos = Test-AuditGroup "Microsoft Windows 10-CyberGovAu-10.2020#UserRights"
-				# }
-				[ReportSection] @{
-					Title = 'Account Policies'
-					AuditInfos = Test-AuditGroup "Microsoft Windows 10-CyberGovAu-10.2020#AccountPolicies"
-				}
-				[ReportSection] @{
-					Title = 'Advanced Audit Policy Configuration'
-					AuditInfos = Test-AuditGroup "Microsoft Windows 10-CyberGovAu-10.2020#AuditPolicies"
 				}
 			)
 		}
@@ -198,28 +175,6 @@
 				[ReportSection] @{
 					Title = 'Security Options'
 					AuditInfos = Test-AuditGroup "Microsoft Windows 10 SiSyPHuS NE-BSI-1.3#SecurityOptions"
-				}
-			)
-		}
-		[ReportSection] @{
-			Title = 'BSI Benchmarks SiM-08202 - BPOL'
-			Description = 'This section contains the BSI Benchmark results.'
-			SubSections = @(
-				[ReportSection] @{
-					Title = 'Registry Settings/Group Policies'
-					AuditInfos = Test-AuditGroup "Microsoft Windows 10 BSI BPOL#RegistrySettings"
-				}
-				[ReportSection] @{
-					Title = 'User Rights Assignment'
-					AuditInfos = Test-AuditGroup "Microsoft Windows 10 BSI BPOL#UserRights"
-				}
-				[ReportSection] @{
-					Title = 'Account Policies'
-					AuditInfos = Test-AuditGroup "Microsoft Windows 10 BSI BPOL#AccountPolicies"
-				}
-				[ReportSection] @{
-					Title = 'Advanced Audit Policy Configuration'
-					AuditInfos = Test-AuditGroup "Microsoft Windows 10 BSI BPOL#AuditPolicies"
 				}
 			)
 		}
