a0ad6b6a4aa1b269c8404e4db5bb995a5a15c820
Steffen Winternheimer <s.winternheimer@fb-pro.com>
2022-11-16 16:29:27 +0100

    Add Windows Server 2022 DC Report (Fix Issue Windows Server 2022 Report includes MS DC Benchmark #170)

--- ATAPAuditor/Reports/Microsoft Windows Server 2022.ps1
+++ ATAPAuditor/Reports/Microsoft Windows Server 2022.ps1
@@ -8,28 +8,6 @@
 	)
 	Sections = @(
 		[ReportSection] @{
-			Title = "Microsoft Benchmarks Domain Controller"
-			Description = "This section contains all benchmarks from Microsoft"
-			SubSections = @(
-				[ReportSection] @{
-					Title = "Registry Settings/Group Policies"
-					AuditInfos = Test-AuditGroup "Microsoft Windows Server 2022 Domain Controller-Microsoft-2022#RegistrySettings"
-				}
-				[ReportSection] @{
-					Title = "User Rights Assignment"
-					AuditInfos = Test-AuditGroup "Microsoft Windows Server 2022 Domain Controller-Microsoft-2022#UserRights"
-				}
-				[ReportSection] @{
-					Title = "Account Policies"
-					AuditInfos = Test-AuditGroup "Microsoft Windows Server 2022 Domain Controller-Microsoft-2022#AccountPolicies"
-				}
-				[ReportSection] @{
-					Title = " Advanced Audit Policy Configuration"
-					AuditInfos = Test-AuditGroup "Microsoft Windows Server 2022 Domain Controller-Microsoft-2022#AuditPolicies"
-				}
-			)
-		}
-		[ReportSection] @{
 			Title = "CIS Benchmarks"
 			Description = "This section contains all benchmarks from CIS"
 			SubSections = @(
