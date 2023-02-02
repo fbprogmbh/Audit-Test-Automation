f4558086f51c4dc8d7ed5ad2ed5c3aeb0cdbdfd0
Steffen Winternheimer <s.winternheimer@fb-pro.com>
2023-02-02 11:56:07 +0100

    Add DISA Windows Server 2022 1.1 AuditGroups ([PR2] Add DISA STIG 1R1 Benchmark for Server 2022 #231)

---
+++ ATAPAuditor/AuditGroups/Microsoft Windows Server 2022-DISA-1.1#SecurityOptions.ps1
@@ -0,0 +1,104 @@
+﻿[AuditTest] @{
+    Id = "V-254445"
+    Task = "Windows Server 2022 must have the built-in guest account disabled."
+    Test = {
+        $securityOption = Get-AuditResource "WindowsSecurityPolicy"
+        $setOption = $securityOption['System Access']["EnableGuestAccount"]
+        
+        if ($null -eq $setOption) {
+            return @{
+                Message = "Currently not set."
+                Status = "False"
+            }
+        }
+        if ($setOption -ne 0) {
+            return @{
+                Message = "'EnableGuestAccount' currently set to: $setOption. Expected: 0"
+                Status = "False"
+            }
+        }
+        
+        return @{
+            Message = "Compliant"
+            Status = "True"
+        }
+    }
+}
+[AuditTest] @{
+    Id = "V-254447"
+    Task = "Windows Server 2022 built-in administrator account must be renamed."
+    Test = {
+        $securityOption = Get-AuditResource "WindowsSecurityPolicy"
+        $setOption = $securityOption['System Access']["NewAdministratorName"]
+        
+        if ($null -eq $setOption) {
+            return @{
+                Message = "Currently not set."
+                Status = "False"
+            }
+        }
+        if ($setOption -notmatch "^(?!Administrator).*$") {
+            return @{
+                Message = "'NewAdministratorName' currently set to: $setOption. Expected: ^(?!Administrator).*$"
+                Status = "False"
+            }
+        }
+        
+        return @{
+            Message = "Compliant"
+            Status = "True"
+        }
+    }
+}
+[AuditTest] @{
+    Id = "V-254448"
+    Task = "Windows Server 2022 built-in guest account must be renamed."
+    Test = {
+        $securityOption = Get-AuditResource "WindowsSecurityPolicy"
+        $setOption = $securityOption['System Access']["NewGuestName"]
+        
+        if ($null -eq $setOption) {
+            return @{
+                Message = "Currently not set."
+                Status = "False"
+            }
+        }
+        if ($setOption -notmatch "^(?!Guest).*$") {
+            return @{
+                Message = "'NewGuestName' currently set to: $setOption. Expected: ^(?!Guest).*$"
+                Status = "False"
+            }
+        }
+        
+        return @{
+            Message = "Compliant"
+            Status = "True"
+        }
+    }
+}
+[AuditTest] @{
+    Id = "V-254465"
+    Task = "Windows Server 2022 must not allow anonymous SID/Name translation."
+    Test = {
+        $securityOption = Get-AuditResource "WindowsSecurityPolicy"
+        $setOption = $securityOption['System Access']["LSAAnonymousNameLookup"]
+        
+        if ($null -eq $setOption) {
+            return @{
+                Message = "Currently not set."
+                Status = "False"
+            }
+        }
+        if ($setOption -ne 0) {
+            return @{
+                Message = "'LSAAnonymousNameLookup' currently set to: $setOption. Expected: 0"
+                Status = "False"
+            }
+        }
+        
+        return @{
+            Message = "Compliant"
+            Status = "True"
+        }
+    }
+}
