b4dffedc9a9044679cd16a5d848404a18e28d552
Steffen Winternheimer <s.winternheimer@fb-pro.com>
2023-02-22 10:58:29 +0100

    Add updated ACSC AuditGroups for "Windows 10" and "Windows 10 Complete" Reports (Update ACSC AuditGroups #209)

---
+++ ATAPAuditor/AuditGroups/Microsoft Windows 10-ACSC-21H1#SecurityOptions.ps1
@@ -0,0 +1,78 @@
+﻿[AuditTest] @{
+    Id = "High-032"
+    Task = "(L1) Ensure 'Accounts: Administrator account status' is set to 'Disabled' (MS only)"
+    Test = {
+        $securityOption = Get-AuditResource "WindowsSecurityPolicy"
+        $setOption = $securityOption['System Access']["EnableAdminAccount"]
+        
+        if ($null -eq $setOption) {
+            return @{
+                Message = "Currently not set."
+                Status = "False"
+            }
+        }
+        if ($setOption -ne 0) {
+            return @{
+                Message = "'EnableAdminAccount' currently set to: $setOption. Expected: 0"
+                Status = "False"
+            }
+        }
+        
+        return @{
+            Message = "Compliant"
+            Status = "True"
+        }
+    }
+}
+[AuditTest] @{
+    Id = "Medium-069"
+    Task = "(L1) Ensure 'Accounts: Guest account status' is set to 'Disabled' (MS only)"
+    Test = {
+        $securityOption = Get-AuditResource "WindowsSecurityPolicy"
+        $setOption = $securityOption['System Access']["EnableGuestAccount"]
+        
+        if ($null -eq $setOption) {
+            return @{
+                Message = "Currently not set."
+                Status = "False"
+            }
+        }
+        if ($setOption -ne 0) {
+            return @{
+                Message = "'EnableGuestAccount' currently set to: $setOption. Expected: 0"
+                Status = "False"
+            }
+        }
+        
+        return @{
+            Message = "Compliant"
+            Status = "True"
+        }
+    }
+}
+[AuditTest] @{
+    Id = "Medium-208"
+    Task = "(L1) Ensure 'Network security: Force logoff when logon hours expire' is set to 'Enabled'"
+    Test = {
+        $securityOption = Get-AuditResource "WindowsSecurityPolicy"
+        $setOption = $securityOption['System Access']["ForceLogoffWhenHourExpire"]
+        
+        if ($null -eq $setOption) {
+            return @{
+                Message = "Currently not set."
+                Status = "False"
+            }
+        }
+        if ($setOption -ne 1) {
+            return @{
+                Message = "'ForceLogoffWhenHourExpire' currently set to: $setOption. Expected: 1"
+                Status = "False"
+            }
+        }
+        
+        return @{
+            Message = "Compliant"
+            Status = "True"
+        }
+    }
+}
