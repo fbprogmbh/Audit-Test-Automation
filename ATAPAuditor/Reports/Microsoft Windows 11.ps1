e809cfd2f04168b2d7e2be1ccf74ed3cd475a45f
Steffen Winternheimer <s.winternheimer@fb-pro.com>
2023-02-23 08:56:56 +0100

    Update Windows Reports (Remove BSI NE and Stand-alone AuditGroups)

--- ATAPAuditor/Reports/Microsoft Windows 11.ps1
+++ ATAPAuditor/Reports/Microsoft Windows 11.ps1
@@ -2,41 +2,13 @@
 	Title = "Windows 11 Report"
 	ModuleName = "ATAPAuditor"
 	BasedOn = @(
-		"CIS Microsoft Windows 11 Stand-alone Benchmark, Version: 1.0.0, Date: 2022-11-15"
 		"CIS Microsoft Windows 11 Enterprise Release 21H2 Benchmark, Version: 21H2, Date: 2022-02-14"
 		"Security baseline for Microsoft Windows 11, Version: 20H2, Date: 2020-12-17"
-		#"Restricted Traffic Limited Functionality Baseline for Microsoft Windows 11, Version: 21H2, Date: 2022-06-18"
 		"BSI SiM-08202 Client unter Windows 10, Version: 1, Date: 2017-09-13"
         "Configuration Recommendations for Hardening of Windows 10 Using Built-in Functionalities: Version 1.3, Date: 2021-05-03"
 		"FB Pro recommendations 'Ciphers Protocols and Hashes Benchmark', Version 1.1.0, Date: 2021-04-15"
 	)
 	Sections = @(
-		[ReportSection] @{
-			Title = "CIS Stand-alone Benchmarks"
-			Description = "This section contains all benchmarks from CIS"
-			SubSections = @(
-				[ReportSection] @{
-					Title = 'Account Policies'
-					AuditInfos = Test-AuditGroup "Microsoft Windows 11-Stand-alone-CIS-1.0.1#AccountPolicies"
-				}
-				[ReportSection] @{
-					Title = 'Advanced Audit Policy Configuration'
-					AuditInfos = Test-AuditGroup "Microsoft Windows 11-Stand-alone-CIS-1.0.1#AuditPolicies"
-				}
-				[ReportSection] @{
-					Title = 'Registry Settings/Group Policies'
-					AuditInfos = Test-AuditGroup "Microsoft Windows 11-Stand-alone-CIS-1.0.1#RegistrySettings"
-				}
-				[ReportSection] @{
-					Title = 'Security Options'
-					AuditInfos = Test-AuditGroup "Microsoft Windows 11-Stand-alone-CIS-1.0.1#SecurityOptions"
-				}
-				[ReportSection] @{
-					Title = 'User Rights Assignment'
-					AuditInfos = Test-AuditGroup "Microsoft Windows 11-Stand-alone-CIS-1.0.1#UserRights"
-				}
-			)
-		}
 		[ReportSection] @{
 			Title = "CIS Benchmarks"
 			Description = "This section contains all benchmarks from CIS"
@@ -89,20 +61,6 @@
 				}
 			)
 		}
-		# [ReportSection] @{
-		# 	Title = "Microsoft Benchmarks Restricted Traffic Limited Functionality Baseline"
-		# 	Description = "This section contains all benchmarks from Microsoft RTLFB"
-		# 	SubSections = @(
-		# 		[ReportSection] @{
-		# 			Title = 'Registry Settings/Group Policies'
-		# 			AuditInfos = Test-AuditGroup "Microsoft Windows 11 Restricted Traffic Limited Functionality Baseline (Machine)-Microsoft-21H2#RegistrySettings"
-		# 		}
-		# 		[ReportSection] @{
-		# 			Title = 'Registry Settings/Group Policies'
-		# 			AuditInfos = Test-AuditGroup "Microsoft Windows 11 Restricted Traffic Limited Functionality Baseline (User)-Microsoft-21H2#RegistrySettings"
-		# 		}
-		# 	)
-		# }
 		[ReportSection] @{
 			Title = 'BSI Benchmarks SySiPHuS Logging'
 			Description = 'This section contains the BSI Benchmark results.'
@@ -128,31 +86,6 @@
 			$domainRole = (Get-CimInstance -Class Win32_ComputerSystem).DomainRole
 		} catch {
 			$domainRole = 99
-		}
-		# if system is Standalone Workstation
-		if ($domainRole -eq 0) {
-			[ReportSection] @{
-				Title = 'BSI Benchmarks SySiPHuS NE'
-				Description = 'This section contains the BSI Benchmark results.'
-				SubSections = @(
-					[ReportSection] @{
-						Title = 'Registry Settings/Group Policies'
-						AuditInfos = Test-AuditGroup "Microsoft Windows 10 SiSyPHuS NE-BSI-1.3#RegistrySettings"
-					}
-					[ReportSection] @{
-						Title = 'User Rights Assignment'
-						AuditInfos = Test-AuditGroup "Microsoft Windows 10 SiSyPHuS NE-BSI-1.3#UserRights"
-					}
-					[ReportSection] @{
-						Title = 'Account Policies'
-						AuditInfos = Test-AuditGroup "Microsoft Windows 10 SiSyPHuS NE-BSI-1.3#AccountPolicies"
-					}
-					[ReportSection] @{
-						Title = 'Security Options'
-						AuditInfos = Test-AuditGroup "Microsoft Windows 10 SiSyPHuS NE-BSI-1.3#SecurityOptions"
-					}
-				)
-			}	
 		}
 		# if system is Member Workstation	
 		if ($domainRole -eq 1) {
@@ -215,37 +148,5 @@
 				)
 			}
 		}
-		# [ReportSection] @{
-		# 	Title = 'BSI Benchmarks SiSyPHus-BSI'
-		# 	Description = 'This section contains the BSI Benchmark results.'
-		# 	SubSections = @(
-		# 		[ReportSection] @{
-		# 			Title = 'Registry Settings/Group Policies'
-		# 			AuditInfos = Test-AuditGroup "Microsoft Windows 10 SiSyPHus-BSI-V1.1#RegistrySettings"
-		# 		}
-		# 	)
-		# }
-		# [ReportSection] @{
-		# 	Title = 'BSI Benchmarks SiSyPHus-BSI Bundespolizei'
-		# 	Description = 'This section contains the BSI Benchmark results.'
-		# 	SubSections = @(
-		# 		[ReportSection] @{
-		# 			Title = 'Registry Settings/Group Policies'
-		# 			AuditInfos = Test-AuditGroup "Microsoft Windows 10-BSI-Bundespolizei#RegistrySettings"
-		# 		}
-		# 		[ReportSection] @{
-		# 			Title = 'User Rights Assignment'
-		# 			AuditInfos = Test-AuditGroup "Microsoft Windows 10-BSI-Bundespolizei#UserRights"
-		# 		}
-		# 		[ReportSection] @{
-		# 			Title = 'Account Policies'
-		# 			AuditInfos = Test-AuditGroup "Microsoft Windows 10-BSI-Bundespolizei#AccountPolicies"
-		# 		}
-		# 		[ReportSection] @{
-		# 			Title = 'Advanced Audit Policy Configuration'
-		# 			AuditInfos = Test-AuditGroup "Microsoft Windows 10-BSI-Bundespolizei#AuditPolicies"
-		# 		}
-		# 	)
-		# }
 	)
 }
