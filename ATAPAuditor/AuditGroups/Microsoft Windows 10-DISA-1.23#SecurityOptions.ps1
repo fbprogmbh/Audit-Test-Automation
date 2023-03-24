108f18b7f30cfcf3ff14756a9f2fbb4c7d995e9f
Steffen Winternheimer <s.winternheimer@fb-pro.com>
2023-03-24 08:46:31 +0100

    Update regex for several AuditGroups #319

--- ATAPAuditor/AuditGroups/Microsoft Windows 10-DISA-1.23#SecurityOptions.ps1
+++ ATAPAuditor/AuditGroups/Microsoft Windows 10-DISA-1.23#SecurityOptions.ps1
@@ -63,9 +63,9 @@
                 Status = "False"
             }
         }
-        if ($setOption -notmatch "^(?!(Administrator|`"Administrator`")).*$") {
+        if ($setOption -notmatch "^(?!.*\bAdministrator\b).*$") {
             return @{
-                Message = "'NewAdministratorName' currently set to: $setOption. Expected: ^(?!(Administrator|`"Administrator`")).*$"
+                Message = "'NewAdministratorName' currently set to: $setOption."
                 Status = "False"
             }
         }
@@ -89,9 +89,9 @@
                 Status = "False"
             }
         }
-        if ($setOption -notmatch "^(?!(Guest|`"Guest`"|Gast|`"Gast`")).*$") {
+        if ($setOption -notmatch "^(?i)(?!.*\b(?:Guest|Gast)\b).*$") {
             return @{
-                Message = "'NewGuestName' currently set to: $setOption. Expected: ^(?!(Guest|`"Guest`"|Gast|`"Gast`")).*$"
+                Message = "'NewGuestName' currently set to: $setOption."
                 Status = "False"
             }
         }
