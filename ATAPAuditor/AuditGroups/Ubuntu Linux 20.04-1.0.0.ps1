bef878f6e90985eb37b61913985636c34909a990
Steffen Winternheimer <s.winternheimer@fb-pro.com>
2023-05-26 15:39:55 +0200

    Add new AuditTests

--- ATAPAuditor/AuditGroups/Ubuntu Linux 20.04-1.0.0.ps1
+++ ATAPAuditor/AuditGroups/Ubuntu Linux 20.04-1.0.0.ps1
@@ -158,3 +158,174 @@
         }
     }
 }
+[AuditTest] @{
+    Id = "1.1.3"
+    Task = "Ensure nodev option set on /tmp partition"
+    Test = {
+        $result = findmnt -n /tmp | grep -v nodev
+
+        if($result -eq $null){
+            return @{
+                Message = "Compliant"
+                Status = "True"
+            }
+        }
+
+        return @{
+            Message = "Not-Compliant"
+            Status = "False"
+        }
+    }
+}
+[AuditTest] @{
+    Id = "1.1.4"
+    Task = "Ensure nosuid option set on /tmp partition"
+    Test = {
+        $result = findmnt -n /tmp | grep -v nosuid
+        
+        if($result -eq $null){
+            return @{
+                Message = "Compliant"
+                Status = "True"
+            }
+        }
+
+        return @{
+            Message = "Not-Compliant"
+            Status = "False"
+        }
+    }
+}
+[AuditTest] @{
+    Id = "1.1.5"
+    Task = "Ensure noexec option set on /tmp partition"
+    Test = {
+        $result = findmnt -n /tmp | grep -v noexec
+        
+        if($result -eq $null){
+            return @{
+                Message = "Compliant"
+                Status = "True"
+            }
+        }
+
+        return @{
+            Message = "Not-Compliant"
+            Status = "False"
+        }
+    }
+}
+[AuditTest] @{
+    Id = "1.1.6"
+    Task = "Ensure /dev/shm is configured"
+    Test = {
+        $result = findmnt -n /dev/shm
+        
+        if($result -match "/dev/shm tmpfs tmpfs rw,nosuid,nodev,noexec"){
+            return @{
+                Message = "Compliant"
+                Status = "True"
+            }
+        }
+
+        return @{
+            Message = "Not-Compliant"
+            Status = "False"
+        }
+    }
+}
+[AuditTest] @{
+    Id = "1.1.7"
+    Task = "Ensure nodev option set on /dev/shm partition"
+    Test = {
+        $result = findmnt -n /dev/shm | grep -v nodev
+        
+        if($result -eq $null){
+            return @{
+                Message = "Compliant"
+                Status = "True"
+            }
+        }
+
+        return @{
+            Message = "Not-Compliant"
+            Status = "False"
+        }
+    }
+}
+[AuditTest] @{
+    Id = "1.1.8"
+    Task = "Ensure nosuid option set on /dev/shm partition"
+    Test = {
+        $result = findnmt -n /dev/shm | grep -v nosuid
+        
+        if($result -eq $null){
+            return @{
+                Message = "Compliant"
+                Status = "True"
+            }
+        }
+
+        return @{
+            Message = "Not-Compliant"
+            Status = "False"
+        }
+    }
+}
+[AuditTest] @{
+    Id = "1.1.9"
+    Task = "Ensure noexec option set on /dev/shm partition"
+    Test = {
+        $result = findmnt -n /dev/shm | grep -v noexec
+        
+        if($result -eq $null){
+            return @{
+                Message = "Compliant"
+                Status = "True"
+            }
+        }
+
+        return @{
+            Message = "Not-Compliant"
+            Status = "False"
+        }
+    }
+}
+[AuditTest] @{
+    Id = "1.1.10"
+    Task = "Ensure separate partition exists for /var"
+    Test = {
+        $result = findmnt /var
+        
+        if($result -match "/var <device> <fstype> rw,relatime,attr2,inode64,noquota"){
+            return @{
+                Message = "Compliant"
+                Status = "True"
+            }
+        }
+
+        return @{
+            Message = "Not-Compliant"
+            Status = "False"
+        }
+    }
+}
+[AuditTest] @{
+    Id = "1.1.11"
+    Task = "Ensure separate partition exists for /var/tmp"
+    Test = {
+        $result = findmnt /var/tmp
+        
+        if($result -match "/var <device> <fstype> rw,relatime,attr2,inode64,noquota"){
+            return @{
+                Message = "Compliant"
+                Status = "True"
+            }
+        }
+
+        return @{
+            Message = "Not-Compliant"
+            Status = "False"
+        }
+    }
+}
