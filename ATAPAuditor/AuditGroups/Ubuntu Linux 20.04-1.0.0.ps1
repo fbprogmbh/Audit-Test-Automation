a05facd01d9bb302c3db7961db69b6e55a937e2e
SteffenWinternheimer <steffen.winternheimer@gmail.com>
2023-05-26 15:21:05 +0200

    Add more AuditTests to Ubuntu

--- ATAPAuditor/AuditGroups/Ubuntu Linux 20.04-1.0.0.ps1
+++ ATAPAuditor/AuditGroups/Ubuntu Linux 20.04-1.0.0.ps1
@@ -1,11 +1,151 @@
 [AuditTest] @{
+    Id = "1.1.1.1"
+    Task = "Ensure mounting of cramfs filesystetms is disabled"
+    Test = {
+        $result1 = modprobe -n -v cramfs | grep -E '(cramfs|install)'
+        $result2 = lsmod | grep cramfs
+        
+        if($result1 -match "install /bin/true" -and $result2 -eq $null){
+            return @{
+                Message = "Compliant"
+                Status = "True"
+            }
+        }
+
+        return @{
+            Message = "Not-Compliant"
+            Status = "False"
+        }
+    }
+}
+[AuditTest] @{
+    Id = "1.1.1.2"
+    Task = "Ensure mounting of freevxfs filesystetms is disabled"
+    Test = {
+        $result1 = modprobe -n -v freevxfs | grep -E '(freevxfs|install)'
+        $result2 = lsmod | grep freevxfs
+        
+        if($result1 -match "install /bin/true" -and $result2 -eq $null){
+            return @{
+                Message = "Compliant"
+                Status = "True"
+            }
+        }
+
+        return @{
+            Message = "Not-Compliant"
+            Status = "False"
+        }
+    }
+}
+[AuditTest] @{
     Id = "1.1.1.3"
     Task = "Ensure mounting of jffs2 filesystetms is disabled"
     Test = {
         $result1 = modprobe -n -v jffs2 | grep -E '(jffs2|install)'
         $result2 = lsmod | grep jffs2
         
-        if($result1 -eq "install /bin/true" -and $result2 -eq $null){
+        if($result1 -match "install /bin/true" -and $result2 -eq $null){
+            return @{
+                Message = "Compliant"
+                Status = "True"
+            }
+        }
+
+        return @{
+            Message = "Not-Compliant"
+            Status = "False"
+        }
+    }
+}
+[AuditTest] @{
+    Id = "1.1.1.4"
+    Task = "Ensure mounting of hfs filesystetms is disabled"
+    Test = {
+        $result1 = modprobe -n -v hfs | grep -E '(hfs|install)'
+        $result2 = lsmod | grep hfs
+        
+        if($result1 -match "install /bin/true" -and $result2 -eq $null){
+            return @{
+                Message = "Compliant"
+                Status = "True"
+            }
+        }
+
+        return @{
+            Message = "Not-Compliant"
+            Status = "False"
+        }
+    }
+}
+[AuditTest] @{
+    Id = "1.1.1.5"
+    Task = "Ensure mounting of hfsplus filesystetms is disabled"
+    Test = {
+        $result1 = modprobe -n -v hfsplus | grep -E '(hfsplus|install)'
+        $result2 = lsmod | grep hfsplus
+        
+        if($result1 -match "install /bin/true" -and $result2 -eq $null){
+            return @{
+                Message = "Compliant"
+                Status = "True"
+            }
+        }
+
+        return @{
+            Message = "Not-Compliant"
+            Status = "False"
+        }
+    }
+}
+[AuditTest] @{
+    Id = "1.1.1.6"
+    Task = "Ensure mounting of squashfs filesystetms is disabled"
+    Test = {
+        $result1 = modprobe -n -v squashfs | grep -E '(squashfs|install)'
+        $result2 = lsmod | grep squashfs
+        
+        if($result1 -match "install /bin/true" -and $result2 -eq $null){
+            return @{
+                Message = "Compliant"
+                Status = "True"
+            }
+        }
+
+        return @{
+            Message = "Not-Compliant"
+            Status = "False"
+        }
+    }
+}
+[AuditTest] @{
+    Id = "1.1.1.7"
+    Task = "Ensure mounting of udf filesystetms is disabled"
+    Test = {
+        $result1 = modprobe -n -v udf | grep -E '(udf|install)'
+        $result2 = lsmod | grep udf
+        
+        if($result1 -match "install /bin/true" -and $result2 -eq $null){
+            return @{
+                Message = "Compliant"
+                Status = "True"
+            }
+        }
+
+        return @{
+            Message = "Not-Compliant"
+            Status = "False"
+        }
+    }
+}
+[AuditTest] @{
+    Id = "1.1.2"
+    Task = "Ensure /tmp is configured"
+    Test = {
+        $result1 = modprobe -n -v udf | grep -E '(udf|install)'
+        $result2 = lsmod | grep udf
+        
+        if($result1 -match "install /bin/true" -and $result2 -eq $null){
             return @{
                 Message = "Compliant"
                 Status = "True"
