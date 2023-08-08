54e7dac974fcbdd7ba600f0ad61ef3be80440713
Swali <52581912+swalbart@users.noreply.github.com>
2023-08-08 12:02:40 +0200

    TPSTFP-112-filter-for-techniques-that-cannot-be-easily-mitigated (#61)
    * Added "noEasyMitigation" to categories.
    
    * Fxed html and Json
    
    * changed names
    
    * fixed the bug where techniques were shown white
    
    * Added missing id to filter as requested.
    
    ---------
    
    Co-authored-by: Tobias Zinke <tobias.zinke@gmx.de>

--- ATAPHtmlReport/ATAPHtmlReport.psm1
+++ ATAPHtmlReport/ATAPHtmlReport.psm1
@@ -1896,6 +1896,13 @@ function Get-ATAPHtmlReport {
 									htmlElement 'input' @{type = "checkbox"; id = "mitreFilterCheckbox"; onchange = "hideMitreTechniques(this)"} {}
 								}
 
+								htmlElement 'p' @{} {
+									htmlElement 'label' @{} {
+										"hide techniques that cannot be easily mitigated with preventive controls:"
+										htmlElement 'input' @{type = "checkbox"; id = "noEasyMitigationCheckbox"; onchange = "noEasyMitigation(this)"} {}
+									}
+								}
+
 								htmlElement 'h2' @{} {"Current ATT&CK heatmap on tested System"}
 
 								ConvertTo-HtmlTable $Mappings.map
--- ATAPHtmlReport/report.js
+++ ATAPHtmlReport/report.js
@@ -257,3 +257,26 @@ function hideMitreTechniques(checkbox){
         }
     }
 }
+
+const noEasyMitigationElements = document.getElementsByClassName('noEasyMitigation');
+function noEasyMitigation(checkbox){
+    if (checkbox.checked) {
+        for (let i = 0; i < noEasyMitigationElements.length; i++) {
+            noEasyMitigationElements[i].style.padding = '0.1em';
+
+            const children = noEasyMitigationElements[i].querySelectorAll('*');
+            for (let j = 0; j < children.length; j++) {
+                children[j].style.display = 'none';
+            }
+        }
+    } else {
+        for (let i = 0; i < noEasyMitigationElements.length; i++) {
+            noEasyMitigationElements[i].style.removeProperty('padding');
+
+            const children = noEasyMitigationElements[i].querySelectorAll('*');
+            for (let j = 0; j < children.length; j++) {
+                children[j].style.removeProperty('display'); 
+            }
+        }
+    }
+}
