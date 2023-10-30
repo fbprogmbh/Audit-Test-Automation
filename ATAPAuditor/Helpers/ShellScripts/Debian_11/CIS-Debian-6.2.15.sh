0305cec7511bf06d5eaa1d93b9f1ac403225dfff
Steffen Winternheimer <s.winternheimer@fb-pro.com>
2023-10-30 11:40:12 +0100

    Update Shell Scripts

--- ATAPAuditor/Helpers/ShellScripts/Debian_11/CIS-Debian-6.2.15.sh
+++ ATAPAuditor/Helpers/ShellScripts/Debian_11/CIS-Debian-6.2.15.sh
@@ -2,19 +2,13 @@
 {
     output=""
     fname=".forward"
-    valid_shells="^($(
-        sed -rn '/^\//{s,/,\\\\/,g;p}' /etc/shells | paste -s -
-        d '|' -
-    ))$"
-    awk -v pat="$valid_shells" -F: '$(NF) ~ pat { print $1 " " $(NF-1) }'
-    /etc/passwd | (
+    valid_shells="^($(sed -rn '/^\//{s,/,\\\\/,g;p}' /etc/shells | paste -s -d '|' - ))$"
+    awk -v pat="$valid_shells" -F: '$(NF) ~ pat { print $1 " " $(NF-1) }' /etc/passwd | (
         while read -r user home; do
-            [ -f "$home/$fname" ] && output="$output\n - User \"$user\" file:
-\"$home/$fname\" exists"
+            [ -f "$home/$fname" ] && output="$output\n - User \"$user\" file: \"$home/$fname\" exists"
         done
         if [ -z "$output" ]; then
-            echo -e "\n-PASSED: - No local interactive users have \"$fname\"
-files in their home directory\n"
+            echo -e "\n-PASSED: - No local interactive users have \"$fname\" files in their home directory\n"
         else
             echo -e "\n- FAILED:\n$output\n"
         fi
