2bd47bf28f477efee49a404a20fe313c9fa87615
Steffen Winternheimer <s.winternheimer@fb-pro.com>
2023-11-27 14:27:24 +0100

    Uncomment memory checks for unix OS

--- ATAPAuditor/Helpers/ReportUnixOS.ps1
+++ ATAPAuditor/Helpers/ReportUnixOS.ps1
@@ -2,7 +2,6 @@
     SoftwareInformation = [SoftwareInformation]@{
         Hostname = hostname
         OperatingSystem = (Get-Content /etc/os-release | Select-String -Pattern '^PRETTY_NAME=\"(.*)\"$').Matches.Groups[1].Value
-        LicenseStatus = $null
         BuildNumber = 'Version {0} (Build {1}.{2})' -f $v.DisplayVersion, $v.CurrentBuildNumber, $v.UBR
         InstallationLanguage = (($(locale) | Where-Object { $_ -match "LANG=" }) -split '=')[1]
         SystemUptime = uptime -p
@@ -15,7 +14,7 @@
         SystemSerialnumber = (dmidecode -t system)[9] | cut -d ':' -f 2 | xargs
         SystemManufacturer = (dmidecode -t system)[6] | cut -d ':' -f 2 | xargs
         SystemModel = $null#(Get-WMIObject -class Win32_ComputerSystem).Model
-        FreeDiskSpace = $null # "{0:N1} GB" -f ((Get-PSDrive | Where-Object { $_.Name -eq '/' }).Free / 1GB)
-        FreePhysicalMemory = $null#"{0:N1} GB" -f (( -split (Get-Content /proc/meminfo | Where-Object { $_ -match 'MemFree:' }))[1] / 1MB)
+        FreeDiskSpace = "{0:N1} GB" -f ((Get-PSDrive | Where-Object { $_.Name -eq '/' }).Free / 1GB)
+        FreePhysicalMemory = "{0:N1} GB" -f (( -split (Get-Content /proc/meminfo | Where-Object { $_ -match 'MemFree:' }))[1] / 1MB)
     }
 }
