649e7a42030607d10ca7c2738f87b9b8368b7c7c
davni-fbp <david.nickel@fb-pro.com>
2023-11-30 14:32:26 +0100

    Correction 6.2.14

--- ATAPAuditor/AuditGroups/SUSE Linux Enterprise 15-CIS-1.1.1.ps1
+++ ATAPAuditor/AuditGroups/SUSE Linux Enterprise 15-CIS-1.1.1.ps1
@@ -3395,8 +3395,9 @@ done
 cut -f3 -d":" /etc/passwd | sort -n | uniq -c | while read x ; do
     [ -z "$x" ] && break
     set - $x
-    if [ $1 -gt 1 ]; then
-        users=$(awk -F: '($3 == n) { print $1 }' n=$2 /etc/passwd | xargs) echo "Duplicate UID ($2): $users"
+    if (( $1 > 1 )); then
+        users=$(awk -F: '($3 == n) { print $1 }' n=$2 /etc/passwd | xargs)
+        echo "Duplicate UID ($2): $users"
     fi
 done
 '@
