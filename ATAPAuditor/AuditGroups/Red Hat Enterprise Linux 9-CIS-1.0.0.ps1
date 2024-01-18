8d25600b41aa1658f6c511988a8d89686ff61d78
davni-fbp <david.nickel@fb-pro.com>
2024-01-18 13:23:42 +0100

    Korrekturen die 2.

--- ATAPAuditor/AuditGroups/Red Hat Enterprise Linux 9-CIS-1.0.0.ps1
+++ ATAPAuditor/AuditGroups/Red Hat Enterprise Linux 9-CIS-1.0.0.ps1
@@ -474,9 +474,9 @@ if ($IPv6Status -match "is enabled") {
 #!/usr/bin/env bash
 {
     l_output="" l_output2=""
-    l_mname="usb-storage" # set module name
-    # Check if the module exists on the system
-    if [ -z "$(modprobe -n -v "$l_mname" 2>&1 | grep -Pi -- "\h*modprobe:\h+FATAL:\h+Module\h+$l_mname\h+not\h+found\h+in\h+directory")" ]; then
+    l_mname="usb-storage"
+    if [ -z "$(modprobe -n -v "$l_mname" 2>&1 | \
+    grep -Pi -- "\h*modprobe:\h+FATAL:\h+Module\h+$l_mname\h+not\h+found\h+in\h+directory")" ]; then
         l_loadable="$(modprobe -n -v "$l_mname")"
         [ "$(wc -l <<< "$l_loadable")" -gt "1" ] && l_loadable="$(grep -P -- "(^\h*install|\b$l_mname)\b" <<< "$l_loadable")"
         if grep -Pq -- '^\h*install \/bin\/(true|false)' <<< "$l_loadable"; then
@@ -484,13 +484,11 @@ if ($IPv6Status -match "is enabled") {
         else
             l_output2="$l_output2\n - module: \"$l_mname\" is loadable: \"$l_loadable\""
         fi
-        # Check is the module currently loaded
         if ! lsmod | grep "$l_mname" > /dev/null 2>&1; then
             l_output="$l_output\n - module: \"$l_mname\" is not loaded"
         else
             l_output2="$l_output2\n - module: \"$l_mname\" is loaded"
         fi
-        # Check if the module is deny listed
         if modprobe --showconfig | grep -Pq -- "^\h*blacklist\h+$(tr '-' '_' <<< "$l_mname")\b"; then
             l_output="$l_output\n - module: \"$l_mname\" is deny listed in: \"$(grep -Pl -- "^\h*blacklist\h+$l_mname\b" /etc/modprobe.d/*)\""
         else
@@ -499,7 +497,6 @@ if ($IPv6Status -match "is enabled") {
     else
         l_output="$l_output\n - Module \"$l_mname\" doesn't exist on the system"
     fi
-    # Report results. If no failures output in l_output2, we pass
     if [ -z "$l_output2" ]; then
         echo -e "\n- Audit Result:\n PASS\n$l_output\n"
     else
