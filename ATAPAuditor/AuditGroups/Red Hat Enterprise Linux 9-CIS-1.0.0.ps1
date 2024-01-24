8ef205ead34cee1ebc2a550205b6dfbaaab28082
davni-fbp <david.nickel@fb-pro.com>
2024-01-24 16:16:35 +0100

    escapes die 6.

--- ATAPAuditor/AuditGroups/Red Hat Enterprise Linux 9-CIS-1.0.0.ps1
+++ ATAPAuditor/AuditGroups/Red Hat Enterprise Linux 9-CIS-1.0.0.ps1
@@ -478,7 +478,7 @@ if ($IPv6Status -match "is enabled") {
     l_output="" l_output2=""
     l_mname="usb-storage"
     if [ -z '$(modprobe -n -v "$l_mname" 2>&1 | grep -Pi -- "\h*modprobe:\h+FATAL:\h+Module\h+$l_mname\h+not\h+found\h+in\h+directory")' ]; then
-        l_loadable="$(modprobe -n -v "$l_mname")"
+        l_loadable='$(modprobe -n -v "$l_mname")'
         [ "$(wc -l <<< "$l_loadable")" -gt "1" ] && l_loadable="$(grep -P -- "(^\h*install|\b$l_mname)\b" <<< "$l_loadable")"
         if grep -Pq -- '^\h*install \/bin\/(true|false)' <<< "$l_loadable"; then
             l_output="$l_output\n - module: \"$l_mname\" is not loadable: \"$l_loadable\""
