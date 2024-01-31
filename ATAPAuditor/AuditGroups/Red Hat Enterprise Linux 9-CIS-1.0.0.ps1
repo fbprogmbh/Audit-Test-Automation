31a90ce2a6bbc6537720b4c87d927c8dfc220140
davni-fbp <david.nickel@fb-pro.com>
2024-01-31 12:35:37 +0100

    123

--- ATAPAuditor/AuditGroups/Red Hat Enterprise Linux 9-CIS-1.0.0.ps1
+++ ATAPAuditor/AuditGroups/Red Hat Enterprise Linux 9-CIS-1.0.0.ps1
@@ -30,6 +30,9 @@ if ($IPv6Status -match "is enabled") {
     $IPv6Status = "disabled"
 }
 
+$parentPath = Split-Path -Parent -Path $PSScriptRoot
+$scriptPath = $parentPath + "/Helpers/ShellScripts/RHEL9/"
+
 
 ### Chapter 1 - Initial Setup
 
@@ -472,42 +475,9 @@ if ($IPv6Status -match "is enabled") {
     Id = "1.1.9"
     Task = "Disable USB Storage"
     Test = {
-        $script_string = @'
-#!/usr/bin/env bash
-{
-    l_output="" l_output2=""
-    l_mname="usb-storage"
-    if [ -z '$(modprobe -n -v "$l_mname" 2>&1 | grep -Pi -- "\h*modprobe:\h+FATAL:\h+Module\h+$l_mname\h+not\h+found\h+in\h+directory")' ]; then
-        l_loadable='$(modprobe -n -v "$l_mname")'
-        [ "$(wc -l <<< "$l_loadable")" -gt "1" ] && l_loadable="$(grep -P -- "(^\h*install|\b$l_mname)\b" <<< "$l_loadable")"
-        if grep -Pq -- '^\h*install \/bin\/(true|false)' <<< "$l_loadable"; then
-            l_output="$l_output\n - module: \"$l_mname\" is not loadable: \"$l_loadable\""
-        else
-            l_output2="$l_output2\n - module: \"$l_mname\" is loadable: \"$l_loadable\""
-        fi
-        if ! lsmod | grep "$l_mname" > /dev/null 2>&1; then
-            l_output="$l_output\n - module: \"$l_mname\" is not loaded"
-        else
-            l_output2="$l_output2\n - module: \"$l_mname\" is loaded"
-        fi
-        if modprobe --showconfig | grep -Pq -- "^\h*blacklist\h+$(tr '-' '_' <<< "$l_mname")\b"; then
-            l_output="$l_output\n - module: \"$l_mname\" is deny listed in: \"$(grep -Pl -- "^\h*blacklist\h+$l_mname\b" /etc/modprobe.d/*)\""
-        else
-            l_output2="$l_output2\n - module: \"$l_mname\" is not deny listed"
-        fi
-    else
-        l_output="$l_output\n - Module \"$l_mname\" doesn't exist on the system"
-    fi
-    if [ -z "$l_output2" ]; then
-        echo -e "\n- Audit Result:\n PASS\n$l_output\n"
-    else
-        echo -e "\n- Audit Result:\n FAIL\n - Reason(s) for audit failure:\n$l_output2\n"
-        [ -n "$l_output" ] && echo -e "\n- Correctly set:\n$l_output\n"
-    fi
-}
-'@
-        $script = bash -c $script_string
-        if ($script -match "PASS") {
+        $resultScript = $scriptPath + "CIS100_RHEL9_119.sh"
+        $result = bash $resultScript
+        if ($result -match "PASS") {
             return $retCompliant
         } else {
             return $retNonCompliant
