fea02d765651f3ffdac1917dea170cf4a4ce126f
davni-fbp <david.nickel@fb-pro.com>
2024-02-01 14:06:29 +0100

    blubb die 2.

--- ATAPAuditor/AuditGroups/Red Hat Enterprise Linux 9-CIS-1.0.0.ps1
+++ ATAPAuditor/AuditGroups/Red Hat Enterprise Linux 9-CIS-1.0.0.ps1
@@ -3340,23 +3340,11 @@ $scriptPath = $parentPath + "/Helpers/ShellScripts/RHEL9/"
     Id = "5.6.5"
     Task = "Ensure default user shell timeout is 900 seconds or less"
     Test = {
-        $script_string1 = @'
-#!/usr/bin/env bash
-{
-    passing=""
-    grep -Eiq '^\s*UMASK\s+(0[0-7][2-7]7|[0-7][2-7]7)\b' /etc/login.defs && grep -Eqi '^\s*USERGROUPS_ENAB\s*"?no"?\b' /etc/login.defs && grep -Eq '^\s*session\s+(optional|requisite|required)\s+pam_umask\.so\b' /etc/pam.d/common-session && passing=true grep -REiq '^\s*UMASK\s+\s*(0[0-7][2-7]7|[0-7][2-7]7|u=(r?|w?|x?)(r?|w?|x?)(r?|w?|x?),g=(r?x?|x?r?),o=)\b' /etc/profile* /etc/bashrc* && passing=true
-    [ "$passing" = true ] && echo "Default user umask is set"
-}
-'@
-    $script_string2 = @'
-#!/usr/bin/env bash
-{
-    grep -RPi '(^|^[^#]*)\s*umask\s+([0-7][0-7][01][0-7]\b|[0-7][0-7][0-7][0-6]\b|[0-7][01][0-7]\b|[0-7][0-7][0-6]\b|(u=[rwx]{0,3},)?(g=[rwx]{0,3},)?o=[rwx]+\b|(u=[rwx]{1,3},)?g=[^rx]{1,3}(,o=[rwx]{0,3})?\b)' /etc/login.defs /etc/profile* /etc/bashrc*
-}
-'@
-        $script1 = bash -c $script_string1
-        $script2 = bash -c $script_string2
-        if ($script1 -match "umask is set" -and $script2 -eq $null) {
+        $resultScript1 = $scriptPath + "CIS100_RHEL9_565_1.sh"
+        $result1 = bash $resultScript1
+        $resultScript2 = $scriptPath + "CIS100_RHEL9_565_2.sh"
+        $result2 = bash $resultScript2
+        if ($result1 -match "umask is set" -and $result2 -eq $null) {
             return $retCompliant
         } else {
             return $retNonCompliant
