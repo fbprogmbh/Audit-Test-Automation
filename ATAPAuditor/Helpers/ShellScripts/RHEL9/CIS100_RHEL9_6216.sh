13f6658cf76c5d9a4fdef6fd298da2c07d17ab3b
davni-fbp <david.nickel@fb-pro.com>
2024-02-06 14:17:03 +0100

    6.2.8 & 6.2.16

--- ATAPAuditor/Helpers/ShellScripts/RHEL9/CIS100_RHEL9_6216.sh
+++ ATAPAuditor/Helpers/ShellScripts/RHEL9/CIS100_RHEL9_6216.sh
@@ -6,7 +6,8 @@
     valid_shells="^($( sed -rn '/^\//{s,/,\\\\/,g;p}' /etc/shells | paste -s -d '|' - ))$"
     awk -v pat="$valid_shells" -F: '$(NF) ~ pat { print $1 " " $(NF-1) }' /etc/passwd | (while read -r user home; do
         for dfile in $(find "$home" -type f -name '.*'); do
-            mode=$( stat -L -c '%#a' "$dfile" ) [ $(( $mode & $perm_mask )) -gt 0 ] && output="$output\n- User $user file: \"$dfile\" is too permissive: \"$mode\" (should be: \"$maxperm\" or more restrictive)"
+            mode=$( stat -L -c '%#a' "$dfile" )
+            [ $(( $mode & $perm_mask )) -gt 0 ] && output="$output\n- User $user file: \"$dfile\" is too permissive: \"$mode\" (should be: \"$maxperm\" or more restrictive)"
         done
     done
     if [ -n "$output" ]; then
--- ATAPAuditor/Helpers/ShellScripts/RHEL9/CIS100_RHEL9_628.sh
+++ ATAPAuditor/Helpers/ShellScripts/RHEL9/CIS100_RHEL9_628.sh
@@ -2,7 +2,8 @@
 {
     RPCV="$(sudo -Hiu root env | grep '^PATH' | cut -d= -f2)"
     echo "$RPCV" | grep -q "::" && echo "root's path contains a empty directory (::)"
-    echo "$RPCV" | grep -q ":$" && echo "root's path contains a trailing (:)" for x in $(echo "$RPCV" | tr ":" " "); do
+    echo "$RPCV" | grep -q ":$" && echo "root's path contains a trailing (:)"
+    for x in $(echo "$RPCV" | tr ":" " "); do
         if [ -d "$x" ]; then
             ls -ldH "$x" | awk '$9 == "." {print "PATH contains current working directory (.)"}
             $3 != "root" {print $9, "is not owned by root"}
