a06ab298982de791180874217f1890be85bce491
davni-fbp <david.nickel@fb-pro.com>
2024-02-13 14:19:34 +0100

    Korrektur 6.2.4

--- ATAPAuditor/AuditGroups/Red Hat Enterprise Linux 9-CIS-1.0.0.ps1
+++ ATAPAuditor/AuditGroups/Red Hat Enterprise Linux 9-CIS-1.0.0.ps1
@@ -3623,7 +3623,8 @@ $scriptPath = $parentPath + "/Helpers/ShellScripts/RHEL9/"
 #!/usr/bin/env bash
 {
     cut -f3 -d":" /etc/passwd | sort -n | uniq -c | while read x ; do
-        [ -z "$x" ] && break set - $x
+        [ -z "$x" ] && break
+        set - $x
         if [ $1 -gt 1 ]; then
             users=$(awk -F: '($3 == n) { print $1 }' n=$2 /etc/passwd | xargs) echo "Duplicate UID ($2): $users"
         fi
