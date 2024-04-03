b7b28727e87eea42dead094b6ddf7fb6baabeec4
svenschrader <sven.schrader@fb-pro.com>
2024-04-03 10:46:10 +0200

    Correction on 6.2.7

--- ATAPAuditor/AuditGroups/SUSE Linux Enterprise 15-CIS-1.1.1.ps1
+++ ATAPAuditor/AuditGroups/SUSE Linux Enterprise 15-CIS-1.1.1.ps1
@@ -3354,20 +3354,9 @@ done
     Id = "6.2.7"
     Task = "Ensure users own their home directories"
     Test = {
-        $test1 = @'
-#!/bin/bash
-grep -E -v '^(halt|sync|shutdown)' /etc/passwd | awk -F: '($7 != "'"$(which nologin)"'" && $7 != "/bin/false") { print $1 " " $6 }' | while read user dir; do
-    if [ ! -d "$dir" ]; then
-        echo "The home directory ($dir) of user $user does not exist."
-    else
-        owner=$(stat -L -c "%U" "$dir")
-        if [ "$owner" != "$user" ]; then
-            echo "The home directory ($dir) of user $user is owned by $owner."
-        fi
-    fi
-done
-'@
-        if($test1 -eq $null){
+        $script1 = $scriptPath + "CIS-SEL15-6.2.7.sh"
+        $test = bash $script1
+        if($test -eq $null){
             return $retCompliant
         } else {
             return $retNonCompliant
