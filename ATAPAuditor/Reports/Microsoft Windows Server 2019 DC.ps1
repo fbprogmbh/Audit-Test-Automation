d2729a6ed4a43e88ec3f30c5d2c7cc8023d8004c
Quang Nguyen <quang.nguyen@fb-pro.com>
2024-08-22 13:28:55 +0200

    Fix Issue#605: Remove DC Reports

--- ATAPAuditor/Reports/Microsoft Windows Server 2019 DC.ps1
+++ ATAPAuditor/Reports/Microsoft Windows Server 2019 DC.ps1
@@ -5,7 +5,6 @@
 	BasedOn = @(
         "DISA Windows Server 2019 Security Technical Implementation Guide, Version: V1R2, Date: 2020-01-24"
         "CIS Microsoft Windows Server 2019 Benchmark, Version: 2.0.0, Date: 2023-04-14"
-        "Microsoft Security baseline for Windows Server 2019 Domain Controller, Version: FINAL, Date 2019-06-18"
         "FB Pro recommendations 'Ciphers Protocols and Hashes Benchmark', Version 1.1.0, Date: 2021-04-15"
         "FB Pro recommendations 'Enhanced settings', Version 1.1.0, Date: 2023-02-24"
 	)
@@ -63,28 +62,6 @@
 				[ReportSection] @{
 					Title = " Advanced Audit Policy Configuration"
 					AuditInfos = Test-AuditGroup "Microsoft Windows Server 2019-CIS-2.0.0#AuditPolicies"
-				}
-			)
-		}
-		[ReportSection] @{
-			Title = "Microsoft Benchmarks"
-			Description = "This section contains all benchmarks from Microsoft"
-			SubSections = @(
-				[ReportSection] @{
-					Title = "Registry Settings/Group Policies"
-					AuditInfos = Test-AuditGroup "Microsoft Windows Server 2019 Domain Controller-Microsoft-FINAL#RegistrySettings"
-				}
-				[ReportSection] @{
-					Title = "User Rights Assignment"
-					AuditInfos = Test-AuditGroup "Microsoft Windows Server 2019 Domain Controller-Microsoft-FINAL#UserRights"
-				}
-				[ReportSection] @{
-					Title = "Account Policies"
-					AuditInfos = Test-AuditGroup "Microsoft Windows Server 2019 Domain Controller-Microsoft-FINAL#AccountPolicies"
-				}
-				[ReportSection] @{
-					Title = " Advanced Audit Policy Configuration"
-					AuditInfos = Test-AuditGroup "Microsoft Windows Server 2019 Domain Controller-Microsoft-FINAL#AuditPolicies"
 				}
 			)
 		}
--- ATAPAuditor/Reports/Microsoft Windows Server 2022 DC.ps1
+++ ATAPAuditor/Reports/Microsoft Windows Server 2022 DC.ps1
@@ -3,34 +3,11 @@
 	Title = "Windows Server 2022 Audit Report for Domain Controller"
 	ModuleName = "ATAPAuditor"
 	BasedOn = @(
-		"Security baseline for Microsoft Windows Server 2022, Version: FINAL, Date 2021-09-27"
 		"CIS Microsoft Windows Server 2022, Version: 2.0.0, Date 2023-04-14"
 		"FB Pro recommendations 'Ciphers Protocols and Hashes Benchmark', Version 1.1.0, Date: 2021-04-15"
 		"FB Pro recommendations 'Enhanced settings', Version 1.1.0, Date: 2023-02-24"
 	)
 	Sections = @(
-		[ReportSection] @{
-			Title = "Microsoft Benchmarks Domain Controller"
-			Description = "This section contains all benchmarks from Microsoft"
-			SubSections = @(
-				[ReportSection] @{
-					Title = "Registry Settings/Group Policies"
-					AuditInfos = Test-AuditGroup "Microsoft Windows Server 2022 Domain Controller-Microsoft-2022#RegistrySettings"
-				}
-				[ReportSection] @{
-					Title = "User Rights Assignment"
-					AuditInfos = Test-AuditGroup "Microsoft Windows Server 2022 Domain Controller-Microsoft-2022#UserRights"
-				}
-				[ReportSection] @{
-					Title = "Account Policies"
-					AuditInfos = Test-AuditGroup "Microsoft Windows Server 2022 Domain Controller-Microsoft-2022#AccountPolicies"
-				}
-				[ReportSection] @{
-					Title = " Advanced Audit Policy Configuration"
-					AuditInfos = Test-AuditGroup "Microsoft Windows Server 2022 Domain Controller-Microsoft-2022#AuditPolicies"
-				}
-			)
-		}
 		[ReportSection] @{
 			Title = "CIS Benchmarks"
 			Description = "This section contains all benchmarks from CIS"
