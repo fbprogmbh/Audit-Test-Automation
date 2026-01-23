ba322ed195528f8b67f9bc3b0a6458de0524bf5d
raw0611 <raw0611@gmail.com>
2026-01-23 13:32:49 +0100

    edited Edge Report

--- ATAPAuditor/Reports/Microsoft Edge.ps1
+++ ATAPAuditor/Reports/Microsoft Edge.ps1
@@ -2,7 +2,7 @@
 	Title = 'Microsoft Edge Report'
 	ModuleName = 'ATAPAuditor'
 	BasedOn = @(
-		"CIS Microsoft Edge Benchmark, Version: 2.0.0, Date: 2023-09-21"
+		"CIS Microsoft Edge Benchmark, Version: 4.0.0, Date: 2025-10-27"
 		"Microsoft Edge v117 Security Baseline FINAL, Version: 117, Date: 2024-04-12"
 	)
 	Sections = @(
@@ -12,7 +12,7 @@
 			SubSections = @(
 				[ReportSection] @{
 					Title = 'Registry Settings/Group Policies'
-					AuditInfos = Test-AuditGroup "Microsoft Edge-CIS-2.0.0#RegistrySettings"
+					AuditInfos = Test-AuditGroup "Microsoft Edge-CIS-4.0.0#RegistrySettings"
 				}
 			)
 		}
