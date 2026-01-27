857489c507d4cce00440a8c7f6a5d516478ecda7
raw0611 <raw0611@gmail.com>
2026-01-27 15:22:53 +0100

    W10 RegistrySettings logic updated + edited reference in report file

--- ATAPAuditor/Reports/Microsoft Windows 10.ps1
+++ ATAPAuditor/Reports/Microsoft Windows 10.ps1
@@ -2,7 +2,7 @@
 	Title = "Windows 10 Report"
 	ModuleName = "ATAPAuditor"
 	BasedOn = @(
-		"CIS Microsoft Windows 10 Enterprise, Version: 3.0.0, Date: 2024-02-22"
+		"CIS Microsoft Windows 10 Enterprise, Version: 4.0.0, Date: 2025-05-23"
 		"Microsoft Security baseline (FINAL) for Windows 10, Version: 21H1, Date: 2021-05-18"
 		"BSI Configuration Recommendations for Hardening of Windows 10 Using Built-in Functionalities: Version 1.3, Date: 2021-05-03"
 		"BSI SiSyPHuS Recommendations for Telemetry Components: Version 1.2, Date: 2020-04-27"
@@ -18,23 +18,23 @@
 			SubSections = @(
 				[ReportSection] @{
 					Title = 'Registry Settings/Group Policies'
-					AuditInfos = Test-AuditGroup "Microsoft Windows 10-CIS-3.0.0#RegistrySettings"
+					AuditInfos = Test-AuditGroup "Microsoft Windows 10-CIS-4.0.0#RegistrySettings"
 				}
 				[ReportSection] @{
 					Title = 'User Rights Assignment'
-					AuditInfos = Test-AuditGroup "Microsoft Windows 10-CIS-3.0.0#UserRights"
+					AuditInfos = Test-AuditGroup "Microsoft Windows 10-CIS-4.0.0#UserRights"
 				}
 				[ReportSection] @{
 					Title = 'Account Policies'
-					AuditInfos = Test-AuditGroup "Microsoft Windows 10-CIS-3.0.0#AccountPolicies"
+					AuditInfos = Test-AuditGroup "Microsoft Windows 10-CIS-4.0.0#AccountPolicies"
 				}
 				[ReportSection] @{
 					Title = 'Advanced Audit Policy Configuration'
-					AuditInfos = Test-AuditGroup "Microsoft Windows 10-CIS-3.0.0#AuditPolicies"
+					AuditInfos = Test-AuditGroup "Microsoft Windows 10-CIS-4.0.0#AuditPolicies"
 				}
 				[ReportSection] @{
 					Title = 'Security Options'
-					AuditInfos = Test-AuditGroup "Microsoft Windows 10-CIS-3.0.0#SecurityOptions"
+					AuditInfos = Test-AuditGroup "Microsoft Windows 10-CIS-4.0.0#SecurityOptions"
 				}
 			)
 		}
