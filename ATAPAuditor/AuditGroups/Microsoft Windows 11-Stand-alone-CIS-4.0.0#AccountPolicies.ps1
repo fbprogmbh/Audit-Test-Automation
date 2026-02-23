7bb28c72a42b832e48dd770deb8915134ad5cf32
raw0611 <raw0611@gmail.com>
2026-02-23 12:50:53 +0100

    AccountPolicies edited #2

--- ATAPAuditor/AuditGroups/Microsoft Windows 11-Stand-alone-CIS-4.0.0#AccountPolicies.ps1
+++ ATAPAuditor/AuditGroups/Microsoft Windows 11-Stand-alone-CIS-4.0.0#AccountPolicies.ps1
@@ -41,7 +41,10 @@
         }
         $setPolicy = [long]$setPolicy
 
-        if ($setPolicy -gt 365 -or $setPolicy -le 0 ) {
+        if (($setPolicy -gt 365 -or $setPolicy -le 0)) {
+            if($setPolicy -eq -1){ #Setting 0 in GroupPolicy translates to -1 in AuditPolicy
+                $setPolicy = "Password never expires"
+            }
             return @{
                 Message = "'MaximumPasswordAge' currently set to: $setPolicy. Expected: x <= 365 and x > 0"
                 Status  = "False"
@@ -181,7 +184,7 @@
         }
         $setPolicy = [long]$setPolicy
 
-        if ($setPolicy -lt 15  -or $setPolicy -gt 99999 ) {
+        if ($setPolicy -lt 15 -or $setPolicy -gt 99999) {
             return @{
                 Message = "'LockoutDuration' currently set to: $setPolicy. Expected: x >= 15 minutes and x <= 99999 minutes"
                 Status  = "False"
@@ -237,7 +240,7 @@
         }
         $setPolicy = [long]$setPolicy
 
-        if ($setPolicy -gt 99999 -or $setPolicy -lt 15 ) {
+        if ($setPolicy -gt 99999 -or $setPolicy -lt 15) {
             return @{
                 Message = "'ResetLockoutCount' currently set to: $setPolicy. Expected: x <= 99999 minutes and x >= 15 minutes"
                 Status  = "False"
