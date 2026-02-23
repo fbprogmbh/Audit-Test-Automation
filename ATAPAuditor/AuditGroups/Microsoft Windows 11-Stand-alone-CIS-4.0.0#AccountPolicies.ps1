d24c4b348bdbf126c563bd451e52f5c77a0e51d2
raw0611 <raw0611@gmail.com>
2026-02-23 12:04:28 +0100

    AccountPolicies 4.0.0 edited

--- ATAPAuditor/AuditGroups/Microsoft Windows 11-Stand-alone-CIS-4.0.0#AccountPolicies.ps1
+++ ATAPAuditor/AuditGroups/Microsoft Windows 11-Stand-alone-CIS-4.0.0#AccountPolicies.ps1
@@ -13,9 +13,9 @@
         }
         $setPolicy = [long]$setPolicy
 
-        if (($setPolicy -ne 24)) {
+        if ($setPolicy -ne 24) {
             return @{
-                Message = "'PasswordHistorySize' currently set to: $setPolicy. Expected: x == 24"
+                Message = "'PasswordHistorySize' currently set to: $setPolicy. Expected: 24"
                 Status  = "False"
             }
         }
@@ -41,9 +41,9 @@
         }
         $setPolicy = [long]$setPolicy
 
-        if (($setPolicy -gt 365 days -or $setPolicy -le 0 days)) {
+        if ($setPolicy -gt 365 -or $setPolicy -le 0 ) {
             return @{
-                Message = "'MaximumPasswordAge' currently set to: $setPolicy. Expected: x <= 365 days and x > 0 days"
+                Message = "'MaximumPasswordAge' currently set to: $setPolicy. Expected: x <= 365 and x > 0"
                 Status  = "False"
             }
         }
@@ -69,9 +69,9 @@
         }
         $setPolicy = [long]$setPolicy
 
-        if (($setPolicy -lt 1 days)) {
+        if ($setPolicy -lt 1) {
             return @{
-                Message = "'MinimumPasswordAge' currently set to: $setPolicy. Expected: x >= 1 days"
+                Message = "'MinimumPasswordAge' currently set to: $setPolicy. Expected: x >= 1"
                 Status  = "False"
             }
         }
@@ -97,7 +97,7 @@
         }
         $setPolicy = [long]$setPolicy
 
-        if (($setPolicy -lt 14)) {
+        if ($setPolicy -lt 14) {
             return @{
                 Message = "'MinimumPasswordLength' currently set to: $setPolicy. Expected: x >= 14"
                 Status  = "False"
@@ -181,7 +181,7 @@
         }
         $setPolicy = [long]$setPolicy
 
-        if (($setPolicy -lt 15 minutes -or $setPolicy -gt 99999 minutes)) {
+        if ($setPolicy -lt 15  -or $setPolicy -gt 99999 ) {
             return @{
                 Message = "'LockoutDuration' currently set to: $setPolicy. Expected: x >= 15 minutes and x <= 99999 minutes"
                 Status  = "False"
@@ -209,7 +209,7 @@
         }
         $setPolicy = [long]$setPolicy
 
-        if (($setPolicy -gt 5 -or $setPolicy -le 0)) {
+        if ($setPolicy -gt 5 -or $setPolicy -le 0) {
             return @{
                 Message = "'LockoutBadCount' currently set to: $setPolicy. Expected: x <= 5 and x > 0"
                 Status  = "False"
@@ -237,7 +237,7 @@
         }
         $setPolicy = [long]$setPolicy
 
-        if (($setPolicy -gt 99999 minutes -or $setPolicy -lt 15 minutes)) {
+        if ($setPolicy -gt 99999 -or $setPolicy -lt 15 ) {
             return @{
                 Message = "'ResetLockoutCount' currently set to: $setPolicy. Expected: x <= 99999 minutes and x >= 15 minutes"
                 Status  = "False"
