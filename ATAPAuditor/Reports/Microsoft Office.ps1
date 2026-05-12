0e522e51fbc8d8d4e2492a414ba98bc4b7cbcb15
raw0611 <raw0611@gmail.com>
2026-05-12 10:15:59 +0200

    added BasedOn Security Baseline in Report file

--- ATAPAuditor/Reports/Microsoft Office.ps1
+++ ATAPAuditor/Reports/Microsoft Office.ps1
@@ -2,8 +2,9 @@
     Title      = 'Microsoft Office Audit Report'
     ModuleName = 'ATAPAuditor'
     BasedOn    = @(
-        'CIS Microsoft Office Enterprise Benchmark, Version: 1.2.0, Date: 2024-07-19'
-    )
+        "CIS Microsoft Office Enterprise Benchmark, Version: 1.2.0, Date: 2024-07-19"
+        "Security Baseline for Microsoft 365 Apps for Enterprise, Version: 2412, Date: 2025-12-15"
+        )
     Sections   = @(
         [ReportSection] @{
             Title       = "CIS Benchmarks"
