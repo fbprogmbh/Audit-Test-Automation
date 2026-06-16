46bb5da8e74f9c168bfd79cb29537a20704bf7a9
raw0611 <raw0611@gmail.com>
2026-06-16 15:18:23 +0200

    Accountpolicies autoformatted

--- ATAPAuditor/AuditGroups/Microsoft Windows 11-Microsoft-22H2#AccountPolicies.ps1
+++ ATAPAuditor/AuditGroups/Microsoft Windows 11-Microsoft-22H2#AccountPolicies.ps1
@@ -1,5 +1,5 @@
 ﻿[AuditTest] @{
-    Id = "AccountPolicy-361"
+    Id   = "AccountPolicy-361"
     Task = "Ensure 'MinimumPasswordLength' is set to '14' character(s)."
     Test = {
         $securityPolicy = Get-AuditResource "WindowsSecurityPolicy"
@@ -8,7 +8,7 @@
         if ($null -eq $setPolicy) {
             return @{
                 Message = "Currently not set."
-                Status = "False"
+                Status  = "False"
             }
         }
         $setPolicy = [long]$setPolicy
@@ -16,18 +16,18 @@
         if ($setPolicy -ne 14) {
             return @{
                 Message = "'MinimumPasswordLength' currently set to: $setPolicy. Expected: 14"
-                Status = "False"
+                Status  = "False"
             }
         }
         
         return @{
             Message = "Compliant"
-            Status = "True"
+            Status  = "True"
         }
     }
 }
 [AuditTest] @{
-    Id = "AccountPolicy-362"
+    Id   = "AccountPolicy-362"
     Task = "The built-in Windows password complexity policy must be enabled."
     Test = {
         $securityPolicy = Get-AuditResource "WindowsSecurityPolicy"
@@ -36,7 +36,7 @@
         if ($null -eq $setPolicy) {
             return @{
                 Message = "Currently not set."
-                Status = "False"
+                Status  = "False"
             }
         }
         $setPolicy = [long]$setPolicy
@@ -44,18 +44,18 @@
         if ($setPolicy -ne 1) {
             return @{
                 Message = "'PasswordComplexity' currently set to: $setPolicy. Expected: 1"
-                Status = "False"
+                Status  = "False"
             }
         }
         
         return @{
             Message = "Compliant"
-            Status = "True"
+            Status  = "True"
         }
     }
 }
 [AuditTest] @{
-    Id = "AccountPolicy-363"
+    Id   = "AccountPolicy-363"
     Task = "The password history must be configured to 24 passwords remembered"
     Test = {
         $securityPolicy = Get-AuditResource "WindowsSecurityPolicy"
@@ -64,7 +64,7 @@
         if ($null -eq $setPolicy) {
             return @{
                 Message = "Currently not set."
-                Status = "False"
+                Status  = "False"
             }
         }
         $setPolicy = [long]$setPolicy
@@ -72,18 +72,18 @@
         if ($setPolicy -ne 24) {
             return @{
                 Message = "'PasswordHistorySize' currently set to: $setPolicy. Expected: 24"
-                Status = "False"
+                Status  = "False"
             }
         }
         
         return @{
             Message = "Compliant"
-            Status = "True"
+            Status  = "True"
         }
     }
 }
 [AuditTest] @{
-    Id = "AccountPolicy-364"
+    Id   = "AccountPolicy-364"
     Task = "Ensure 'LockoutBadCount' is set to '10' invalid logon attempt(s)"
     Test = {
         $securityPolicy = Get-AuditResource "WindowsSecurityPolicy"
@@ -92,7 +92,7 @@
         if ($null -eq $setPolicy) {
             return @{
                 Message = "Currently not set."
-                Status = "False"
+                Status  = "False"
             }
         }
         $setPolicy = [long]$setPolicy
@@ -100,18 +100,18 @@
         if ($setPolicy -ne 10) {
             return @{
                 Message = "'LockoutBadCount' currently set to: $setPolicy. Expected: 10"
-                Status = "False"
+                Status  = "False"
             }
         }
         
         return @{
             Message = "Compliant"
-            Status = "True"
+            Status  = "True"
         }
     }
 }
 [AuditTest] @{
-    Id = "AccountPolicy-365"
+    Id   = "AccountPolicy-365"
     Task = "Ensure 'Reset account lockout counter after' is set to '10 minutes'"
     Test = {
         $securityPolicy = Get-AuditResource "WindowsSecurityPolicy"
@@ -120,7 +120,7 @@
         if ($null -eq $setPolicy) {
             return @{
                 Message = "Currently not set."
-                Status = "False"
+                Status  = "False"
             }
         }
         $setPolicy = [long]$setPolicy
@@ -128,18 +128,18 @@
         if ($setPolicy -ne 10) {
             return @{
                 Message = "'ResetLockoutCount' currently set to: $setPolicy. Expected: 10 minutes"
-                Status = "False"
+                Status  = "False"
             }
         }
         
         return @{
             Message = "Compliant"
-            Status = "True"
+            Status  = "True"
         }
     }
 }
 [AuditTest] @{
-    Id = "AccountPolicy-366"
+    Id   = "AccountPolicy-366"
     Task = "Ensure 'LockoutDuration' is set to '10 minutes'"
     Test = {
         $securityPolicy = Get-AuditResource "WindowsSecurityPolicy"
@@ -148,7 +148,7 @@
         if ($null -eq $setPolicy) {
             return @{
                 Message = "Currently not set."
-                Status = "False"
+                Status  = "False"
             }
         }
         $setPolicy = [long]$setPolicy
@@ -156,18 +156,18 @@
         if ($setPolicy -ne 10) {
             return @{
                 Message = "'LockoutDuration' currently set to: $setPolicy. Expected: 10 minutes"
-                Status = "False"
+                Status  = "False"
             }
         }
         
         return @{
             Message = "Compliant"
-            Status = "True"
+            Status  = "True"
         }
     }
 }
 [AuditTest] @{
-    Id = "AccountPolicy-367"
+    Id   = "AccountPolicy-367"
     Task = "Reversible password encryption must be disabled."
     Test = {
         $securityPolicy = Get-AuditResource "WindowsSecurityPolicy"
@@ -176,7 +176,7 @@
         if ($null -eq $setPolicy) {
             return @{
                 Message = "Currently not set."
-                Status = "False"
+                Status  = "False"
             }
         }
         $setPolicy = [long]$setPolicy
@@ -184,13 +184,13 @@
         if ($setPolicy -ne 0) {
             return @{
                 Message = "'ClearTextPassword' currently set to: $setPolicy. Expected: 0"
-                Status = "False"
+                Status  = "False"
             }
         }
         
         return @{
             Message = "Compliant"
-            Status = "True"
+            Status  = "True"
         }
     }
 }
