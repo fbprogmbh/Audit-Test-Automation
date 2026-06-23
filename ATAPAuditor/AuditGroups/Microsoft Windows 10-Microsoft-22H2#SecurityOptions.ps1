a16139a5f8e04f4c36f3b68a74100d406ad3b822
raw0611 <raw0611@gmail.com>
2026-06-23 09:55:11 +0200

    SecurityOptions autoformatted

--- ATAPAuditor/AuditGroups/Microsoft Windows 10-Microsoft-22H2#SecurityOptions.ps1
+++ ATAPAuditor/AuditGroups/Microsoft Windows 10-Microsoft-22H2#SecurityOptions.ps1
@@ -3,24 +3,24 @@
     Task = "Ensure 'Network access: Allow anonymous SID/Name translation' is set to 'Disabled'"
     Test = {
         $securityOption = Get-AuditResource "WindowsSecurityPolicy"
-                $setOption = $securityOption['System Access']["LSAAnonymousNameLookup"]
+        $setOption = $securityOption['System Access']["LSAAnonymousNameLookup"]
         
-                if ($null -eq $setOption) {
-                    return @{
-                        Status  = "False"
-                        Message = "Currently not set."
-                    }
-                }
-                if ($setOption -ne 0) {
-                    return @{
-                        Message = "'LSAAnonymousNameLookup' currently set to: $setOption. Compliant value: 0"
-                        Status  = "False"
-                    }
-                }
+        if ($null -eq $setOption) {
+            return @{
+                Status  = "False"
+                Message = "Currently not set."
+            }
+        }
+        if ($setOption -ne 0) {
+            return @{
+                Message = "'LSAAnonymousNameLookup' currently set to: $setOption. Compliant value: 0"
+                Status  = "False"
+            }
+        }
         
-                return @{
-                    Message = "Compliant"
-                    Status  = "True"
-                }
+        return @{
+            Message = "Compliant"
+            Status  = "True"
+        }
     }
 }
